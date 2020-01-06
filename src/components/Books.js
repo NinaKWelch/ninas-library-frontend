c7433e2cacf44b3f2457b5861d714357a8ffb22f
NinaKWelch <ninakwelch@gmail.com>
2020-01-06 14:18:22 +0000

    tehtava 8.19

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Books = ({ show, result }) => {
+  const [genre, setGenre] = useState('all')
+
   if (!show) {
     return null
   }
@@ -8,6 +10,28 @@ const Books = ({ show, result }) => {
   if (result.loading) {
     return <div>Loading...</div>
   }
+
+  const listGenders = () => {
+    // create a single array of all added genres
+    const allGenres = result.data.allBooks.map(a => a.genres).flat()
+    // remove duplicates
+    // https://stackoverflow.com/questions/9229645/remove-duplicate-values-from-js-array
+    const uniqueGenres = allGenres.reduce(function(a, b) {
+      if (a.indexOf(b) < 0) a.push(b);
+        return a
+      }, []
+    )
+
+    return uniqueGenres.map(g =>
+      <button key={g} onClick={() => setGenre(g)}>
+        {g}
+      </button>
+    )
+  }
+
+  const filterByGenre = genre => genre !== 'all'
+    ? result.data.allBooks.filter(a => a.genres.includes(genre))
+    : result.data.allBooks
 
   return (
     <div>
@@ -24,7 +48,7 @@ const Books = ({ show, result }) => {
               Published
             </th>
           </tr>
-          {result.data.allBooks.map(a =>
+          {filterByGenre(genre).map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -33,6 +57,11 @@ const Books = ({ show, result }) => {
           )}
         </tbody>
       </table>
+
+      <div>
+        {listGenders()}
+        <button onClick={() => setGenre('all')}>all genres</button>
+      </div>
     </div>
   )
 }
